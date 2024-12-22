c0798189d6db47cd3e31dd73f7c4f3a427184976
JALIKRULES <JALIKSHOOTER@gmail.com>
2024-12-22 13:08:18 +0500

    AI is smarter now
    - It can avoid other AIs and prioritize what is important and decide what is important at state of ivertaking : to overtake or follow waypoints

--- TOP DOWN RACER/Assets/Sources/Scripts C#/AI/CarAIHandler.cs
+++ TOP DOWN RACER/Assets/Sources/Scripts C#/AI/CarAIHandler.cs
@@ -8,13 +8,18 @@ public class CarAIHandler : MonoBehaviour
     [Header("AI settings")]
     public AIMode aiMode;
     public float maxSpeed = 16f;
+    public bool isAvoidingCars = true;
 
 
     Vector3 targetPosition = Vector3.zero;
     Transform targetTransform = null;
 
+    Vector2 avoidanceVectorLerped = Vector2.zero;
+
     WaypointNode currentWaypoint = null;
     WaypointNode[] allWaypoints;
+
+    PolygonCollider2D polygonCollider2D;
 
     TopDownCarController topDownCarController;
 
@@ -22,6 +27,8 @@ public class CarAIHandler : MonoBehaviour
     {
         topDownCarController = GetComponent<TopDownCarController>();
         allWaypoints = FindObjectsOfType<WaypointNode>();
+
+        polygonCollider2D = GetComponentInChildren<PolygonCollider2D>();
     }
 
     private void FixedUpdate()
@@ -84,6 +91,9 @@ public class CarAIHandler : MonoBehaviour
         Vector2 vectorToTarget = targetPosition - transform.position;
         vectorToTarget.Normalize();
 
+        if (isAvoidingCars)
+            AvoidCars(vectorToTarget, out vectorToTarget);
+
         float angleToTarget = Vector2.SignedAngle(transform.up, vectorToTarget);
         angleToTarget *= -1;
 
@@ -100,5 +110,66 @@ public class CarAIHandler : MonoBehaviour
             return 0;
 
         return 1.05f - Mathf.Abs(inputX) / 1.0f;
+    }
+
+    bool IsCarInFrontOfAICar(out Vector3 position, out Vector3 otherCarRightVector)
+    {
+        polygonCollider2D.enabled = false;
+
+        RaycastHit2D raycastHit2D = Physics2D.CircleCast(transform.position + transform.up * 0.5f, 1.2f, transform.up, 12, 1 << LayerMask.NameToLayer("Car"));
+
+        polygonCollider2D.enabled = true;
+
+        if (raycastHit2D.collider != null)
+        {
+            Debug.DrawRay(transform.position, transform.up * 12, Color.red);
+
+            position = raycastHit2D.collider.transform.position;
+            otherCarRightVector = raycastHit2D.collider.transform.right;
+
+            return true;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.up * 12, Color.black);
+
+        }
+
+        position = Vector3.zero;
+        otherCarRightVector = Vector3.zero;
+
+        return false;
+    }
+
+    void AvoidCars(Vector2 vectorToTarget, out Vector2 newVectorToTarget)
+    {
+        if (IsCarInFrontOfAICar(out Vector3 otherCarPosition, out Vector3 otherCarRightVector))
+        {
+            Vector2 avoidanceVector = Vector2.zero;
+
+            avoidanceVector = Vector2.Reflect((otherCarPosition - transform.position).normalized, otherCarRightVector - transform.position);
+
+            float distanceToTarget = (targetPosition - transform.position).magnitude;
+
+            float driveToTargetInfluence = 6.0f / distanceToTarget;
+
+            driveToTargetInfluence = Mathf.Clamp(driveToTargetInfluence, 0.30f, 1.0f);
+
+            float avoidanceInfluence = 1.0f - driveToTargetInfluence;
+
+            avoidanceVectorLerped = Vector2.Lerp(avoidanceVectorLerped, avoidanceVector, Time.fixedDeltaTime * 4);
+
+            newVectorToTarget = vectorToTarget * driveToTargetInfluence + avoidanceVectorLerped * avoidanceInfluence;
+            newVectorToTarget.Normalize();
+
+            Debug.DrawRay(transform.position, avoidanceVector * 10, Color.green);
+
+            Debug.DrawRay(transform.position, newVectorToTarget * 10, Color.yellow);
+
+            return;
+        }
+
+        newVectorToTarget = vectorToTarget;
+
     }
 }
